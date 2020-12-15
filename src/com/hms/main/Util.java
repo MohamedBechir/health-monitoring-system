942217d4731368caf37dfd07bf6065ed71f0c036
Mootez <mootez.saad@medtech.tn>
2020-12-15 10:47:23 +0100

    Reimplement populating route points

--- src/com/hms/main/Util.java
+++ src/com/hms/main/Util.java
@@ -2,7 +2,6 @@ package com.hms.main;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 
 public class Util {	
@@ -22,25 +21,12 @@ public class Util {
   }
 
   public static List<Point> pointsGenerator() {
-	int x1 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int y1 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int x2 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int y2 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int x3 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int y3 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int x4 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int y4 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int x6 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int y6 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int x7 = 2 + (int)(Math.random() * ((6 - 2) + 1));
-	int y7 = 2 + (int)(Math.random() * ((6 - 2) + 1));
+	
     List<Point> points = new ArrayList<Point>();
-    points.add(new Point(x1,y1));
-    points.add(new Point(x2,y2));
-    points.add(new Point(x3,y3));
-    points.add(new Point(x4,y4));
-    points.add(new Point(x6,y6));
-    points.add(new Point(x7,y7));
+    
+    for(int i = 0; i < 7; i++) {
+      points.add(new Point(2 + (int)(Math.random() * ((6 - 2) + 1)), 2 + (int)(Math.random() * ((6 - 2) + 1))));
+    }
 
     return points;
   }
